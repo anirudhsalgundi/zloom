2a6cfef8007331d4a6ccca2ba9941ce61b1c2694
anirudhsalgundi <134363781+anirudhsalgundi@users.noreply.github.com>
2026-03-03 18:29:51 -0500

    Refactor choose_filter.py to improve function naming and structure

--- src/zloom/filters/choose_filter.py
+++ src/zloom/filters/choose_filter.py
@@ -1,13 +1,13 @@
-from zloom.all_filters import all_filters
+from zloom.filters import all_filters
 
 
-def get_available_filters() -> list:
+def _get_available_filters() -> list:
     filters = [f for f in all_filters.keys()]
     print("Available filters:", filters)
     return filters
 
 
-def choose_filter(filters) -> str:
+def _choose_filter(filters) -> str:
     while True:
         choice = input("Enter the name of the filter you want to use: ")
         if choice in filters:
@@ -16,3 +16,20 @@ def choose_filter(filters) -> str:
             return active_filter
         else:
             print("Invalid choice. Please try again.")
+
+
+def choose_filter() -> str:
+    filter_names = _get_available_filters()
+    if not filter_names:
+        print("No filters available.")
+        return None
+    else:
+        chosen_filter = _choose_filter(filter_names)
+        user_filter = all_filters[chosen_filter]
+        return user_filter
+
+def main():
+    choose_filter()
+
+if __name__ == "__main__":
+    main()
