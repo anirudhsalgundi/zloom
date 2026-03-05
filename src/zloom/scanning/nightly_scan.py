fb03fd7c9e8d71aaf35559caa5c17ee564c945bc
anirudhsalgundi <134363781+anirudhsalgundi@users.noreply.github.com>
2026-03-05 18:23:35 -0500

    - nightly_scan scans for last x days up to 7 days
    - Added functions to scan ZTF data
    
    - Added function to scan both ZTF and LSST data that passed the filter.

--- src/zloom/scanning/nightly_scan.py
+++ src/zloom/scanning/nightly_scan.py
@@ -1,47 +1,43 @@
-import sys
 import os
-from astropy.time import Time
-from zloom.filters import all_filters
-from zloom.lsst import get_alert_count
-from zloom.auth import get_boom_token
-from zloom.filters.choose_filter import choose_filter
+import shutil
 import requests
-from collections import defaultdict
-import json
-from astropy.time import Time
 import webbrowser
 import argparse
+from astropy.time import Time
+from zloom.auth import get_boom_token
+from zloom.filters import all_filters
+from zloom.utils import get_logger, log_banner
+from zloom.filters.choose_filter import choose_filter
+from zloom.lsst.alert_count import get_alert_count as lsst_alert_count
+from zloom.ztf.alert_count import get_alert_count as ztf_alert_count
 
 
-import logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-)
-logger = logging.getLogger(__name__)
+logger = get_logger(__name__)
+width = shutil.get_terminal_size().columns
 
-
-def setup(n_days: float = 1, credentials_file_path: str = None) -> tuple:
+def _setup(n_days: float) -> tuple:
     api_token = get_boom_token()
-    n_alerts = get_alert_count(n_days) #default to 1 day
-    return api_token, n_alerts
+    print("-" * width)
+    lsst_alert_count(n_days)
+    ztf_alert_count(n_days)
+    print("-" * width)
+    return api_token
 
 
-def argument_parser() -> argparse.Namespace:
+def _argument_parser() -> argparse.Namespace:
     """
     Parses command-line arguments for the BOOM alerts script.
     """
 
     parser = argparse.ArgumentParser(description="Play with BOOM alerts")
-    parser.add_argument("--creds", required=False, default=os.environ.get("BOOM_CREDS"))
-    parser.add_argument("--filter_name", required=False, help="If you already know which exact filter you want to use, you can specify it here. Otherwise, the script will prompt you to choose from the available filters.")
+    parser.add_argument("--filter_name", required=False, help="Name of your filer that you saved in filters/all_filters.py (For eg. ). If you already know which exact filter you want to use, you can specify it here. Otherwise, the script will prompt you to choose from the available filters.")
     parser.add_argument("--n_tabs", required=False, default=3, type=int, help="Number of tabs to open at once when viewing Babamul pages. After opening each batch, the script will prompt you to continue or stop.")
-    parser.add_argument("--n_days", required=False, default=1, type=float, help="Number of days to look back when counting alerts and filtering. Default is 1 day.")
+    parser.add_argument("--n_days", required=False, default=1, type=float, help="Number of days to look back when counting alerts and filtering. Default is 1 day. [CANNOT BE MORE THAN 7 DAYS]")
+    parser.add_argument("--survey", required=False, default="both", choices=["LSST", "ZTF", "both"], help="Survey to filter alerts from. Default is LSST.")
 
     return parser.parse_args()
 
-def filter_rubin_alerts(api_token: str, user_filter: list, n_days: float) -> dict:
-    # user_filter.replace("true", "True").replace("false", "False")
+def _filter_lsst_alerts(api_token: str, user_filter: list, n_days: float) -> dict:
     response = requests.post(
                             "https://api.kaboom.caltech.edu/filters/test",
                             headers={
@@ -62,18 +58,19 @@ def filter_rubin_alerts(api_token: str, user_filter: list, n_days: float) -> dic
     try:
         filtered_alerts = response.json()
     except Exception:
-        print("response text:", response.text)
+        log_banner(logger, f"response text: {response.text}")
         filtered_alerts = None
 
     if response.status_code != 200:
-        print(f"Error: {response.status_code}")
-        print(response.text)
+        log_banner(logger, f"Error: {response.status_code}")
+        log_banner(logger, response.text)
+        return None
 
 
-    print (f"Number of alerts returned by filter: {len(filtered_alerts)}")
+    log_banner(logger, f"Number of alerts that passed filter in LSST: {len(filtered_alerts)}")
     return filtered_alerts
 
-def filter_ztf_alerts(api_token: str, user_filter: list, n_days: float) -> dict:
+def _filter_ztf_alerts(api_token: str, user_filter: list, n_days: float) -> dict:
     response = requests.post(
                             "https://api.kaboom.caltech.edu/filters/test",
                             headers={
@@ -84,29 +81,45 @@ def filter_ztf_alerts(api_token: str, user_filter: list, n_days: float) -> dict:
                             "end_jd": Time.now().jd,
                             "limit": None,
                             "pipeline": user_filter,
-                            "permissions": {},
+                            "permissions": {"ZTF": [1,2]},
                             "sort_by": None,
                             "sort_order": "Ascending",
                             "start_jd": Time.now().jd - n_days,
-                            "survey": "LSST"}
+                            "survey": "ZTF"}
                         )
 
     try:
         filtered_alerts = response.json()
     except Exception:
-        print("response text:", response.text)
+        log_banner(logger, "response text:", response.text)
         filtered_alerts = None
 
     if response.status_code != 200:
-        print(f"Error: {response.status_code}")
-        print(response.text)
+        log_banner(logger, f"Error: {response.status_code}")
+        log_banner(logger, response.text)
+        return None
 
 
-    print (f"Number of alerts returned by filter: {len(filtered_alerts)}")
+    log_banner(logger, f"Number of alerts that passed filter in ZTF: {len(filtered_alerts)}")
     return filtered_alerts
 
 
-def get_unique_object_ids(filtered_alerts: dict) -> list:
+def _filter_ztf_and_lsst_alerts(api_token: str, user_filter: list, n_days: float) -> dict:
+
+    lsst_alerts = _filter_lsst_alerts(api_token, user_filter, n_days)
+    ztf_alerts = _filter_ztf_alerts(api_token, user_filter, n_days)
+
+    combined = {
+        "LSST": _get_unique_object_ids(lsst_alerts) if lsst_alerts else [],
+        "ZTF": _get_unique_object_ids(ztf_alerts) if ztf_alerts else []
+    }
+
+    log_banner(logger, f"Number of sources that passed the filter in LSST: {len(combined['LSST'])}")
+    log_banner(logger, f"Number of sources that passed the filter in ZTF: {len(combined['ZTF'])}")
+    return combined
+
+
+def _get_unique_object_ids(filtered_alerts: dict) -> list:
     results = filtered_alerts["data"]["results"]
 
     object_ids = [i["objectId"] for i in results]
@@ -117,50 +130,68 @@ def get_unique_object_ids(filtered_alerts: dict) -> list:
 
 
 # FIXME: when boom is ported to fritz, this function should move from babamul to fritz
-def view_babamul_page(unique_object_ids: str, n_tabs) -> None:
+def _view_babamul_page(survey: str, unique_object_ids: str, n_tabs) -> None:
 
     for start in range(0, len(unique_object_ids), n_tabs):
         chunk = unique_object_ids[start:start + n_tabs]
         for i in chunk:
-            url = f"https://babamul.caltech.edu/objects/LSST/{i}"
+            url = f"https://babamul.caltech.edu/objects/{survey}/{i}"
             webbrowser.open_new_tab(url)
         
         # Ask before continuing (skip prompt after last batch)
         if start + n_tabs < len(unique_object_ids):
-            response = input(f"Opened {min(start + n_tabs, len(unique_object_ids))}/{len(unique_object_ids)}. Open next 20? (y/n): ")
+            response = input(f"Opened {min(start + n_tabs, len(unique_object_ids))}/{len(unique_object_ids)}. Open next {n_tabs}? (y/n): ")
             if response.lower() != 'y':
-                print("Stopped.")
-                break
+                print(f"Cannot identify input {response}. Valid options are: 'y' or 'n'")
+            if response.lower() == 'n':
+                print("Stopping.")
+                return
+               
 
     print("Done!")
 
 
-
-def main():
-    
-
-    args = argument_parser()
-    api_token, n_alerts = setup(n_days=args.n_days, credentials_file_path=args.creds)
+def scan():
+    args = _argument_parser()
+    api_token = _setup(args.n_days)
 
     if not args.filter_name:
-        # filters = get_available_filters()
         active_filter = choose_filter()
-
     else:
         active_filter = all_filters[args.filter_name]
 
-    # logger.info(f"Using filter: {active_filter}")
-    filtered_alerts = filter_rubin_alerts(api_token, active_filter, n_days=args.n_days)
-    unique_object_ids = get_unique_object_ids(filtered_alerts)
-    print(f"Unique object IDs from filtered alerts: {len(unique_object_ids)}")
+
+    if args.survey == "LSST":
+        filtered_alerts = _filter_lsst_alerts(api_token, active_filter, n_days=args.n_days)
+    elif args.survey == "ZTF":
+        filtered_alerts = _filter_ztf_alerts(api_token, active_filter, n_days=args.n_days)
+    elif args.survey == "both":
+        combined_alerts = _filter_ztf_and_lsst_alerts(api_token, active_filter, n_days=args.n_days)
+        if len(combined_alerts["LSST"]) == 0 and len(combined_alerts["ZTF"]) == 0:
+            log_banner(logger, "No alerts passed the filter in either survey. Exiting.")
+            print("\n\n***** Scan complete! *****")
+            return
+        else: 
+            for survey_name, ids in combined_alerts.items():
+                log_banner(logger, f"\nOpening {survey_name} objects...")
+
+                _view_babamul_page(survey_name, ids, args.n_tabs)
+                print("\n\n***** Scan complete! *****")
+        return
+
+    unique_object_ids = _get_unique_object_ids(filtered_alerts)
+    log_banner(logger, f"Unique object IDs from filtered alerts: {len(unique_object_ids)}")
     
-    if not args.n_tabs:
-        n_tabs = 3
-    else:       
-        n_tabs = args.n_tabs
     
-    view_babamul_page(unique_object_ids, n_tabs)
+    _view_babamul_page(args.survey, unique_object_ids, args.n_tabs)
+
+    print("\n\n***** Scan complete! *****")
     return None
+
+
+
+def main():
+    scan()
 
 if __name__ == "__main__":
     main()
